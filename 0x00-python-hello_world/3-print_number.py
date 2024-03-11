4bb51a750c125d3e41c2deb24fdb3a21cd83ce85
JosepharDev <josephardev@gmail.com>
2024-03-11 15:22:10 -0700

    add

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3
+File Edit Options Buffers Tools Python Help                                     
+#!/usr/bin/python3                                                              
 number = 98
 print("{:d} Battery street".format(number))
