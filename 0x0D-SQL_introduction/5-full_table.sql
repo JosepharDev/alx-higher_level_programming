be7712d1551a64f2475add4e6c7db836541ee8ce
JosepharDev <josephardev@gmail.com>
2024-05-23 11:04:16 -0700

    0-privileges

--- 0x0D-SQL_introduction/5-full_table.sql
+++
@@ -1,5 +0,0 @@
--- script that prints the full description of the table first_table
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
